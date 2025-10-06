8c5e3c63112a6e2a93fe112c489ad397a11e3eb5
Za-boss <jasonflake895@gmail.com>
2025-10-06 13:00:02 -0600

    mydb done, test skeleton for squirrel server tests made

--- test_mydb.py
+++ test_mydb.py
@@ -91,7 +91,6 @@ def describe_test_my_db():
 
             os.remove("test.db")
         def load_strings_works_with_empty_strings():
-
             a_db = MyDB("test.db")
 
             a_list = [""]
@@ -102,6 +101,147 @@ def describe_test_my_db():
 
             assert a_res_list == a_list
 
+
+            os.remove("test.db")
+    def describe_save_strings_functionality():
+        def it_successfully_saves_string_arrays():
+            a_db = MyDB("test.db")
+            a_list = ["some", "very", "gummy", "worms"]
+
+            a_db.saveStrings(a_list)
+
+            a_res_list = a_db.loadStrings()
+
+            assert a_res_list == a_list
+
+            os.remove("test.db")  
+        def it_saves_non_string_types():
+            a_db = MyDB("test.db")
+            a_list = [5, False, True, None]
+
+            a_db.saveStrings(a_list)
+
+            a_res_list = a_db.loadStrings()
+
+            assert a_res_list == [5, False, True, None]
+
+            os.remove("test.db")  
+        def it_saves_very_large_string_arrays():
+            a_db = MyDB("test.db")
+
+            a_list = [
+                "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+                "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+                "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yam", "zucchini",
+                "apricot", "blackberry", "blueberry", "cranberry", "dragonfruit", "eggplant",
+                "feijoa", "guava", "hackberry", "imbe", "jackfruit", "kumquat", "lime", "mulberry",
+                "navel_orange", "olive", "peach", "pear", "pineapple", "plum", "pomegranate", "pumpkin",
+                "rhubarb", "soursop", "tomato", "ugni", "voavanga", "wolfberry", "xylocarp", "yuzu",
+                "ziziphus", "almond", "brazilnut", "cashew", "chestnut", "coconut", "hazelnut",
+                "macadamia", "pecan", "pistachio", "walnut", "acorn", "beechnut", "candlenut", "ginkgo",
+                "hickory", "kapok", "monkey_puzzle", "pine_nut", "sheanut", "tiger_nut", "water_chestnut",
+                "breadfruit", "cantaloupe", "durian", "entawak", "figberry", "genip", "huckleberry",
+                "ilang", "jaboticaba", "langsat", "longan", "mamey", "nance", "okra", "persimmon",
+                "quenepa", "rambutan", "safou", "tamarind", "ume", "voavanga_fruit", "wax_apple",
+                "ximenia", "yellow_passionfruit", "zebra_tomato", "angelica", "basil", "cinnamon",
+                "dill", "eucalyptus", "fennel", "garlic", "horseradish", "ivy_gourd", "jalapeno",
+                "kelp", "lavender", "mint", "nutmeg", "oregano", "parsley", "rosemary", "sage",
+                "thyme", "tarragon", "vanilla_bean", "wasabi", "yarrow", "zatar"
+            ]
+
+            a_db.saveStrings(a_list)
+
+            a_res_list = a_db.loadStrings()
+
+            assert a_res_list == a_list
+
+            os.remove("test.db")
+        def two_db_handles_can_write_to_same_db():
+            a_db = MyDB("test.db")
+            b_db = MyDB("test.db")
+            a_list = ["I","go","first"]
+            b_list = ["and", "then", "I", "go"]
+
+            a_db.saveStrings(a_list)
+            b_db.saveStrings(b_list)
+
+            a_res_list = a_db.loadStrings()
+            b_res_list = b_db.loadStrings()
+
+            assert b_list == a_res_list and b_list == b_res_list
+
+            os.remove("test.db")       
+    def describe_save_string_functionality():
+        def it_succesfully_saves_empty_strings():
+            a_db = MyDB("test.db")
+
+            a_str = ""
+
+            a_db.saveString(a_str)
+
+            a_res_list = a_db.loadStrings()
+
+            assert a_res_list == [""]
+
+            os.remove("test.db")
+        def it_saves_non_string_types():
+            a_db = MyDB("test.db")
+
+            a_str = None
+            b_str = False
+            c_str = True
+            d_str = 5
+
+            a_db.saveString(a_str)
+            a_db.saveString(b_str)
+            a_db.saveString(c_str)
+            a_db.saveString(d_str)
+
+            a_res_list = a_db.loadStrings()
+
+            assert a_res_list == [None, False, True, 5]
+
+            os.remove("test.db")
+        def it_saves_very_large_strings():
+            a_db = MyDB("test.db")
+
+            a_str = (
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "
+                * 50  
+            )
+
+            a_db.saveString(a_str)
+
+            a_res_list = a_db.loadStrings()
+
+            assert a_res_list == [(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "
+                * 50  
+            )]
+
+            os.remove("test.db")
+        def two_db_handles_can_write_to_same_db():
+            a_db = MyDB("test.db")
+            b_db = MyDB("test.db")
+
+            a_str = "First write"
+            b_str = "Second write"
+
+            a_db.saveString(a_str)
+            b_db.saveString(b_str)
+
+            a_res_list = a_db.loadStrings()
+            b_res_list = b_db.loadStrings()
+
+            assert a_res_list == [a_str, b_str] and b_res_list == [a_str, b_str]
 
             os.remove("test.db")
 
--- test_squirrel_server.py
+++ test_squirrel_server.py
@@ -1,4 +1,60 @@
 import requests
+
+def describe_squirrel_server_functionality():
+    def describe_handle_squirrels_index_functionality():
+        def it_returns_200_success_code():
+            pass
+        def it_returns_correct_headers():
+            pass
+        def it_sends_squirrels_list():
+            pass
+    def describe_handle_squirrels_retrieve_functionality():
+        def it_returns_200_on_good_request():
+            pass
+        def it_returns_correct_headers():
+            pass
+        def it_returns_a_single_squirrel():
+            pass
+        def it_returns_404_on_malformed_url_request():
+            pass
+        def it_returns_404_on_malformed_id_request():
+            pass
+    def describe_handle_squirrels_create_functionality():
+        def it_returns_201_success_code():
+            pass
+        def it_returns_correct_headers():
+            pass
+        def it_actually_creates_the_squirrel():
+            pass
+    def describe_handle_squirrels_update_functionality():
+        def it_returns_204_on_good_request():
+            pass
+        def it_actually_updates_the_squirrel():
+            pass
+        def it_returns_correct_headers():
+            pass
+        def it_returns_404_on_malformed_url_request():
+            pass
+        def it_returns_404_on_malformed_id_request():
+            pass
+    def describe_handle_squirrels_delete_functionality():
+        def it_returns_204_on_good_request():
+            pass
+        def it_actually_deletes_the_squirrel():
+            pass
+        def it_returns_correct_headers():
+            pass
+        def it_returns_404_on_malformed_url_request():
+            pass
+        def it_returns_404_on_malformed_id_request():
+            pass
+    def describe_handle_404_functionaltiy():
+        def it_returns_404_on_malformed_url_request():
+            pass
+        def it_returns_404_on_malformed_id_request():
+            pass
+        def it_returns_correct_headers():
+            pass
 """
 response = requests.get("http://localhost:8080/squirrels")
 
